f9f095dbb341d39a56129473e066015b412d3853
parkerSchenck <parker.schenck1@gmail.com>
2024-06-20 12:59:18 -0400

    100%

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 // JavaScript goes here.
+
+let yesButton = document.querySelector(".yes-button");
+let noButton = document.querySelector(".no-button");
+let submitButton = document.querySelector(".submit-button");
+let resultDiv = document.querySelector(".result");
+let questionButtonValue = 0;
+let casualImg = querySelector(".casual-img");
+let connoissuerImg = querySelector(".connoisseur-img");
+let gatekeeperImg = querySelector(".gatekeeper-img");
+let expertImg = querySelector(".expert-img");
+
+
+
+
+//When clicked, the value increases to 1.
+yesButton.addEventListener("click", function(){
+    questionButtonValue = 1;
+    console.log(questionButtonValue);
+
+});
+
+//When clicked, the value remains at 0.
+noButton.addEventListener("click", function(){
+    questionButtonValue = 0;
+    connsole.log(questionButtonValue);
+
+});
+
+submitButton.addEventListener("click", function(){
+    let recordNum = document.querySelector(".record-num").value;
+
+    if (recordNum <= 6 && questionButtonValue === 0) {
+        resultDiv.innerHTML = "You're a Casual Listener. Nothing bad about that at all, you chose to enjoy music while its on.";
+        casualImg.style.display = 'block';
+
+    } else if (recordNum > 6 && questionButtonValue === 0) {
+        resultDiv.innerHTML = "You're a Music Connoissuer. You enjoy music to it's fullest extent while understanding the taste of others.";
+        connoisseurImg.style.display = 'block';
+
+    } else if (recordNum <= 6 && questionButtonValue === 1) {
+        resultDiv.innerHTML = "You're a Music Gatekeeper. You don't enjoy letting others know what to listen to, instead you keep it to yourself.";
+        gatekeeperImg.style.display = 'block';
+
+    } else if (recordNum > 6 && questionButtonValue === 1){
+        resultDiv.innerHTML = "You're a Music Expert I guess... You believe that you have a more superior music taste than everyone else.";
+        expertImg.style.display = 'block';
+
+    }
+    
+});
